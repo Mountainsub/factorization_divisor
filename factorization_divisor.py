893c6111b68ad396c6b2a333c5a9137cd72eabc6
YUDAI YAMASHITA <70077254+yudaiyamashita@users.noreply.github.com>
2021-03-22 20:26:05 +0900

    Update factorization_divisor.py

--- factorization_divisor.py
+++ factorization_divisor.py
@@ -1,4 +1,4 @@
-import manup
+def sample_code():
     n = 2**2 * 3**2 * 5**2
 
     print('# 素因数')
@@ -8,3 +8,33 @@ import manup
     print('# 約数')
     for div in divisorize(fct):
         print(div, num(div))
+
+
+def divisorize(fct):
+    b, e = fct.pop() # base, exponen
+    pre_div = divisorize(fct) if fct else [[]]
+    suf_div = [[(b, k)] for k in range(e+1)]
+    return [pre + suf for pre in pre_div for suf in suf_div]
+
+def factorize(n):
+    fct = []
+    b, e = 2,0
+    while b * b <= n:
+        while n % b == 0:
+            n = n // b
+            e += 1
+        if e > 0:
+            fct.append((b, e))
+        b, e = b+ 1, 0
+    if n > 1:
+        fct.append((n, 1))
+    return fct
+
+def num(fct):
+    a = 1
+    for base, exponent in fct:
+        a = a * base**exponent
+    return a
+
+if __name__== '__main__':
+    sample_code()
